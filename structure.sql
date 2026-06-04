7ccdf4a4abd12289cdbc6fd320972b82020b4fc3
Mariel <maxngh01@gmail.com>
2026-06-04 03:01:41 +0100

    Creation de la structure SQL de la base de donnees

---
+++ structure.sql
@@ -0,0 +1,27 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    telephone VARCHAR(20) NOT NULL UNIQUE,
+    filiere VARCHAR(10) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    bio TEXT
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE skills (
+    id SERIAL PRIMARY KEY,
+    nom_matiere VARCHAR(100) NOT NULL UNIQUE,
+);
+
+CREATE TABLE user_mentors (
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    skill_id INT REFERENCES skills(id) ON DELETE CASCADE,
+    PRIMARY KEY(user_id,skill_id)
+);
+
+CREATE TABLE user_mentees (
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    skill_id INT REFERENCES skills(id) ON DELETE CASCADE,
+    PRIMARY KEY(user_id,skill_id)
+):
